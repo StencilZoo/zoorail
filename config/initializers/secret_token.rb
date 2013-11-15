657b5c859d46c61aec43c717822dbca8596ad4c5
StencilZoo <stencilzoo.info@gmail.com>
2013-11-15 02:48:15 +0000

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zoorail::Application.config.secret_key_base = 'c3fba14d5b992c8c68a5f8da5dbd27d2787f71afe538fa185b5147d9d0cef991f568145853501ebcc874ec07526a02b1e15df33b45645863d34fb38917065f76'
